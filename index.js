62d5d6833cdc75c486e8be6a39a866c66fe4ee13
Andres2498 <helan2498@gmail.com>
2023-08-01 11:26:00 -0500

    DeployBackConRail

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ const port = process.env.PORT || 3001
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(port, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
 });
